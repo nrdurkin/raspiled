8d94ea39d3679f4b4a5262938b86aae8e7740c56
Nicholas Durkin <nrdurkin3@gmail.com>
2021-05-18 17:21:56 -0400

    ripple mode tweaking

--- modes/ripple.py
+++ modes/ripple.py
@@ -40,7 +40,7 @@ class RippleCtrl:
             b+=c[2]* weight
             t += weight
 
-        _decay = 1.5
+        _decay = .5
         return max(r/t - _decay, 0), max(g/t - _decay,0), max(b/t - _decay,0)
 
     def draw(self):
